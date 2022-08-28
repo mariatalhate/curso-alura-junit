973666e53b12a980a241827fc69b99fe363abe2b
Maria <talhatemaria@gmail.com>
2022-08-28 20:51:03 -0300

    Final do curso :)

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/modelo/BonusServiceTest.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/modelo/BonusServiceTest.java
@@ -9,19 +9,18 @@ import br.com.alura.tdd.modelo.Funcionario;
 class BonusServiceTest {
 
 	@Test
-	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
-		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Maria", LocalDate.now(), new BigDecimal("25000")));
-
-		assertEquals(BigDecimal.ZERO, bonus);
-	}
-
-	@Test
 	void bonusDeveriaSer10PorCentoDoSalario() {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Maria", LocalDate.now(), new BigDecimal("2500")));
 
 		assertEquals(new BigDecimal("250.0"), bonus);
+	}
+	
+	@Test
+	void bonusNaoDeveriaExistir() {
+		BonusService service = new BonusService();
+		assertThrows(IllegalArgumentException.class, 
+				() -> service.calcularBonus(new Funcionario("Maria", LocalDate.now(), new BigDecimal("11000"))));
 	}
 
 	
--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/modelo/ReajusteServiceTest.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/modelo/ReajusteServiceTest.java
@@ -3,43 +3,60 @@ package br.com.alura.tdd.modelo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ReajusteServiceTest {
 
+	private ReajusteService service;
+	private Funcionario funcionario;
+	
+	@BeforeAll
+	public static void antesDeTodos() {
+		System.out.println("Inicio.");
+	}
+	
+	@AfterAll
+	public static void depoisDeTodos() {
+		System.out.println("Depois de todos.");
+	}
+	
+	@BeforeEach
+	public void inicializar(){
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("Maria", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+	
+	@AfterEach
+	public void finalizar(){
+		System.out.println("Fim.");
+	}
+
 	@Test
 	public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Maria", LocalDate.now(), new BigDecimal("1000.00"));
 		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
 
 	@Test
 	public void reajusteDeveriaSerDeQuinzePorcentoQuandoDesempenhoForBom() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Maria", LocalDate.now(), new BigDecimal("1000.00"));
 		service.concederReajuste(funcionario, Desempenho.BOM);
-
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
 
 	@Test
 	public void reajusteDeveriaSerDeVintePorcentoQuandoDesempenhoForOtimo() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Maria", LocalDate.now(), new BigDecimal("1000.00"));
 		service.concederReajuste(funcionario, Desempenho.OTIMO);
-
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 
 	@Test
 	public void reajusteDeveriaSerDeQuarentaPorcentoQuandoDesempenhoForEspetacular() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Maria", LocalDate.now(), new BigDecimal("1000.00"));
 		service.concederReajuste(funcionario, Desempenho.ESPETACULAR);
-
 		assertEquals(new BigDecimal("1400.00"), funcionario.getSalario());
 	}
 
